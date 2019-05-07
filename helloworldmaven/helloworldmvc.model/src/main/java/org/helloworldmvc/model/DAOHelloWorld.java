174a64f2c771c71d65d75ba51d03b607cac1e9f1
etienne <etienne.brocherieux@viacesi.fr>
2019-05-07 11:26:43 +0200

    phase 2 ++

--- helloworldmaven/helloworldmvc.model/src/main/java/org/helloworldmvc/model/DAOHelloWorld.java
+++ helloworldmaven/helloworldmvc.model/src/main/java/org/helloworldmvc/model/DAOHelloWorld.java
@@ -1,37 +1,49 @@
 package org.helloworldmvc.model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class DAOHelloWorld {
-  
+class DAOHelloWorld {
+    private static final String  FileName          = "HelloWorld.txt";
+    private static DAOHelloWorld instance          = null;
+    private String               helloWorldMessage = null;
 
-	
-	private String FileName = "C://Users//etibr//git//WS_5//helloworldmaven/HelloWorld.txt";
-	private DAOHelloWorld instance;
-	private String HelloWorldMessage;
-	
-	
-	public String getHelloWorldMessage() {
-		return HelloWorldMessage;
-	}
-	public void setHelloWorldMessage(String helloWorldMessage) {
-		HelloWorldMessage = helloWorldMessage;
-	}
-	public DAOHelloWorld getInstance() {
-		return instance;
-	}
-	public void setInstance(DAOHelloWorld instance) {
-		this.instance = instance;
-	}
-	
-	public void readfile() {
+    private DAOHelloWorld() {
+        this.readFile();
+    }
 
-	}
-	
-	
-	
-	}
-		
+    public static synchronized DAOHelloWorld getInstance() {
+        if (instance == null) {
+            setInstance(new DAOHelloWorld());
+        }
+        return instance;
+    }
 
+    private static void setInstance(final DAOHelloWorld instance) {
+        DAOHelloWorld.instance = instance;
+    }
 
-    
+    private void readFile() {
+        BufferedReader buffer;
+        try {
+            buffer = new BufferedReader(new InputStreamReader(new FileInputStream(FileName)));
+            this.setHelloWorldMessage(buffer.readLine());
+            buffer.close();
+        } catch (final FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (final IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 
+    public String getHelloWorldMessage() {
+        return this.helloWorldMessage;
+    }
+
+    private void setHelloWorldMessage(final String helloWorldMessage) {
+        this.helloWorldMessage = helloWorldMessage;
+    }
+}
